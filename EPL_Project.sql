356a40bbba655bb8b18e697e7d17ef80e5bfee97
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2023-08-13 23:39:21 -0400

    updated ERD diagram

--- EPL_Project.sql
+++ EPL_Project.sql
@@ -23,6 +23,18 @@ CREATE TABLE Matches (
     HTAG INT,
     HTR CHAR(1),
     Referee VARCHAR(50),
+    AC INT,
+    AF INT,
+    AR INT,
+    AY INT,
+    `AS` INT,
+    AST INT,
+    HC INT,
+    HF INT,
+    HR INT,
+    HY INT,
+    HS INT,
+    HST INT,
     FOREIGN KEY (HomeTeam) REFERENCES Team(TeamID),
     FOREIGN KEY (AwayTeam) REFERENCES Team(TeamID)
 );
@@ -51,9 +63,8 @@ CREATE TABLE HalfTimeResults (
 CREATE TABLE Odds (
     OddsID INT AUTO_INCREMENT PRIMARY KEY,
     MatchID INT,
-    HomeTeamOdds FLOAT,
-    DrawOdds FLOAT,
-    AwayTeamOdds FLOAT,
+	FullTimeResultsID INT,
+    HalfTimeResultsID INT, 
     B365H FLOAT,
     B365D FLOAT,
     B365A FLOAT,
@@ -78,64 +89,17 @@ CREATE TABLE Odds (
     AvgH FLOAT,
     AvgD FLOAT,
     AvgA FLOAT,
-    B365Over25 FLOAT,
-    B365Under25 FLOAT,
-    POver25 FLOAT,
-    PUnder25 FLOAT,
-    MaxOver25 FLOAT,
-    MaxUnder25 FLOAT,
-    AvgOver25 FLOAT,
-    AvgUnder25 FLOAT,
-    AHh FLOAT,
-    B365AHH FLOAT,
-    B365AHA FLOAT,
-    PAHH FLOAT,
-    PAHA FLOAT,
-    MaxAHH FLOAT,
-    MaxAHA FLOAT,
-    AvgAHH FLOAT,
-    AvgAHA FLOAT,
-    FOREIGN KEY (MatchID) REFERENCES Matches(MatchID)
+    FOREIGN KEY (MatchID) REFERENCES Matches(MatchID),
+    FOREIGN KEY (FullTimeResultsID) REFERENCES FullTimeResults(ResultID),
+    FOREIGN KEY (HalfTimeResultsID) REFERENCES HalfTimeResults(ResultID)
 );
 
--- Betting Table
-CREATE TABLE Betting (
-    BettingID INT AUTO_INCREMENT PRIMARY KEY,
+-- Asian Handicap Betting Table
+CREATE TABLE AsianHandicapBetting (
+    AsianHandicapBettingID INT AUTO_INCREMENT PRIMARY KEY,
     MatchID INT,
     FullTimeResultsID INT,
-    HalfTimeResultsID INT, -- Added column for HalfTimeResultsID
-    B365H FLOAT,
-    B365D FLOAT,
-    B365A FLOAT,
-    BWH FLOAT,
-    BWD FLOAT,
-    BWA FLOAT,
-    IWH FLOAT,
-    IWD FLOAT,
-    IWA FLOAT,
-    PSH FLOAT,
-    PSD FLOAT,
-    PSA FLOAT,
-    WHH FLOAT,
-    WHD FLOAT,
-    WHA FLOAT,
-    VCH FLOAT,
-    VCD FLOAT,
-    VCA FLOAT,
-    MaxH FLOAT,
-    MaxD FLOAT,
-    MaxA FLOAT,
-    AvgH FLOAT,
-    AvgD FLOAT,
-    AvgA FLOAT,
-    B365Over25 FLOAT,
-    B365Under25 FLOAT,
-    POver25 FLOAT,
-    PUnder25 FLOAT,
-    MaxOver25 FLOAT,
-    MaxUnder25 FLOAT,
-    AvgOver25 FLOAT,
-    AvgUnder25 FLOAT,
+    HalfTimeResultsID INT, 
     AHh FLOAT,
     B365AHH FLOAT,
     B365AHA FLOAT,
@@ -147,7 +111,26 @@ CREATE TABLE Betting (
     AvgAHA FLOAT,
     FOREIGN KEY (MatchID) REFERENCES Matches(MatchID),
     FOREIGN KEY (FullTimeResultsID) REFERENCES FullTimeResults(ResultID),
-    FOREIGN KEY (HalfTimeResultsID) REFERENCES HalfTimeResults(ResultID) -- Added foreign key constraint for HalfTimeResultsID
+    FOREIGN KEY (HalfTimeResultsID) REFERENCES HalfTimeResults(ResultID) 
+);
+
+-- Over Under Betting Table
+CREATE TABLE OverUnderBetting (
+    OverUnderBettingID INT AUTO_INCREMENT PRIMARY KEY,
+    MatchID INT,
+    FullTimeResultsID INT,
+    HalfTimeResultsID INT, 
+    B365Over25 FLOAT,
+    B365Under25 FLOAT,
+    POver25 FLOAT,
+    PUnder25 FLOAT,
+    MaxOver25 FLOAT,
+    MaxUnder25 FLOAT,
+    AvgOver25 FLOAT,
+    AvgUnder25 FLOAT,
+    FOREIGN KEY (MatchID) REFERENCES Matches(MatchID),
+    FOREIGN KEY (FullTimeResultsID) REFERENCES FullTimeResults(ResultID),
+    FOREIGN KEY (HalfTimeResultsID) REFERENCES HalfTimeResults(ResultID) 
 );
 
 
