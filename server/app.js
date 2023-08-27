4afc26eb20999c63e1399d893970d888181bb761
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2023-08-27 17:29:23 -0400

    enabled CORS on backend

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import matchRoutes from './routes/matchRoutes.js';
 import dropdownRoutes from './routes/dropdownRoutes.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use('/api/matches', matchRoutes);
 app.use('/api/dropdowns', dropdownRoutes);
 
